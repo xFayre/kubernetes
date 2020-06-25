7b1ff2e7d8ec2c4130013ec803a2ceab6f8c0cf9
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-06-25 17:52:27 -0300

    Scripts numerados na sequência me que são executados no momento do Provisionamento

--- install/kubeadm/multipass/provision.sh
+++ install/kubeadm/multipass/provision.sh
@@ -11,20 +11,20 @@ if [ ! -e environment.conf ]; then
 fi
 
 provision() {
-  # ./set-multipass-cidr-in-environment.conf-file.sh
-  . environment.conf
-  . ./generate-cloud-init-files.sh
-  . ./create-servers.sh
-  $(./set-environment-variables-with-servers-information.sh)
-  . ./setup-netplan.sh
-  . ./setup-hosts-file.sh
-  . ./setup-dns-bind.sh
-  . ./restart-servers.sh
-  . ./setup-loadbalancer-haproxy.sh
-  . ./update-system-config.sh
-  . ./update-local-etc-hosts.sh
-  . ./setup-cri-containerd.sh
-  . ./setup-masters-tools.sh
+  . ./00-set-multipass-cidr-in-environment.conf-file.sh
+  . ./environment.conf
+  . ./01-generate-cloud-init-files.sh
+  . ./02-create-servers.sh
+  $(./03-set-environment-variables-with-servers-information.sh)
+  . ./04-setup-netplan.sh
+  . ./05-setup-hosts-file.sh
+  . ./06-setup-dns-bind.sh
+  . ./07-restart-servers.sh
+  . ./08-setup-loadbalancer-haproxy.sh
+  . ./09-update-system-config.sh
+  . ./10-update-local-etc-hosts.sh
+  . ./11-setup-cri-containerd.sh
+  . ./12-setup-masters-tools.sh
 
   printf 'Provision finished in %d hour %d minute %d seconds\n' $((${SECONDS}/3600)) $((${SECONDS}%3600/60)) $((${SECONDS}%60))
 }
