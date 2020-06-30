e90942341668fab8820bed915709ac3ec9fb07ae
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-06-30 15:24:18 -0300

    Nova pasta para exemplos e instalação silenciosa do containerd

--- install/kubeadm/vagrant/ubuntu/install-containerd.sh
+++ install/kubeadm/vagrant/ubuntu/install-containerd.sh
@@ -18,25 +18,25 @@ sysctl --system > /dev/null
 
 ## Set up the repository
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update -q && \
-  apt-get install -y -qq \
+apt-get update -qq && \
+  apt-get install -y -qqq \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
 ## Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - &> /dev/null
 
 ## Add Docker apt repository.
-add-apt-repository \
+add-apt-repository -q \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release --codename --short) \
-    stable"
+    stable" &> /dev/null
 
 ## Install containerd
-apt-get update -q && \
-apt-get install -y -qq containerd.io
+apt-get update -qq && \
+apt-get install -y -qqq containerd.io
 
 # Configure containerd
 mkdir -p /etc/containerd && \
@@ -45,4 +45,4 @@ containerd config default > /etc/containerd/config.toml
 # Restart containerd
 systemctl restart containerd
 
-containerd --version
+containerd --version | awk '{ print $2, $3 }'
