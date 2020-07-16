057ff932ab6958e4485228fc71442f472b6cd008
Silvio Silva <silvio.silva-ext3@ab-inbev.com>
2020-07-16 11:31:30 -0300

    Refatorando comandos do exemplo com Telepresence

--- lab/telepresence/run.sh
+++ lab/telepresence/run.sh
@@ -1,3 +1,8 @@
+
+if grep --quiet "There is no local cluster named" <<< $(minikube status); then
+  minikube start
+fi
+
 kubectl config use-context minikube
 
 kubectl create namespace dev
@@ -10,28 +15,4 @@ docker pull datawire/hello-world
 docker pull datawire/telepresence-k8s:0.105-30-g8a9aa4e
 docker pull yauritux/busybox-curl
 
-cat <<EOF > headless-hello-service.yaml
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: host-hello
-spec:
-  ports:
-    - protocol: TCP
-      port: 80
-      targetPort: 8000
----
-apiVersion: v1
-kind: Endpoints
-metadata:
-  name: host-hello
-subsets:
-  - addresses:
-      - ip: 172.17.0.1
-    ports:
-      - port: 8000
-EOF
-
 watch -n 2 kubectl get deploy,cm,rs,pods,svc,ep -o wide
-
